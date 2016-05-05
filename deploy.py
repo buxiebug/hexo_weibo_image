7823f50708dfe5d3b3f9c5f7882ef1c16785b361
brian.yang <brian.yang@qunar.com>
2016-05-05 15:33:27 +0800

    use cp -rf to override file

--- deploy.py
+++ deploy.py
@@ -20,7 +20,6 @@ if db_str.strip() == "":
     db_str = "{}"
 image_db_dict = json.loads(db_str)
 
-os.system("rm -rf backup/_posts/")
 os.system("cp -rf source/_posts backup/")
 for i in os.listdir(base_dir + '/_posts'):
     path = base_dir + '/_posts/' + i
@@ -56,7 +55,6 @@ with open(image_db, 'w') as f:
 
 os.system('sh install.sh')
 print "copy begin"
-os.system('rm -rf source/_posts')
 os.system('cp -rf backup/_posts source/')
 print "copy done"
 os.chdir("source/_posts")
